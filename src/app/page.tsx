6bb84389b4a9a954d01e284c3df8c5ca3cc51f94
Djordannn <djordanfnby@gmail.com>
2024-11-05 12:09:31 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import {
 import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 
 import js from "/public/image/js.png";
 import html from "/public/image/html.png";
@@ -25,11 +26,11 @@ import node from "/public/image/node.png";
 import react from "/public/image/react.png";
 import project from "/public/image/project.jpg";
 
-// interface IProject {
-//   name: string;
-//   teks: string;
-//   imgUrl: string;
-// }
+interface IProject {
+  name: string;
+  teks: string;
+  imgUrl: string;
+}
 
 // interface ITools {
 //   img: string;
@@ -90,7 +91,7 @@ const dataTools = [
 export default function Home() {
   const newDataProject = dataProject.map((value, index) => {
     return (
-      <CarouselItem key={index} className="basis-1/3">
+      <CarouselItem key={index} className="md:basis-1/3">
         <Card className="w-[100%] bg-[#3c3c3c] text-[#eeee] border-none shadow-lg border-2 border-[#b1d690]">
           <CardHeader>
             <Image src={value.imgUrl} alt="" />
@@ -124,40 +125,11 @@ export default function Home() {
       >
         <div className="text-2xl">Logo</div>
         <div>
-          <ul className="flex gap-6">
-            <li>
-              <Link
-                href="#"
-                className="active border-2 border-[#B1D690] py-2 px-4 rounded-lg"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#about"
-                className="border-2 border-transparent py-2 px-4 rounded-lg hover:border-[#B1D690]"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#myProject"
-                className="border-2 border-transparent py-2 px-4 rounded-lg hover:border-[#B1D690] ease-in-out duration-100"
-              >
-                Project
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#tools"
-                className="border-2 border-transparent py-2 px-4 rounded-lg hover:border-[#b1d690] ease-in-out duration-100"
-              >
-                Tools
-              </Link>
-            </li>
-          </ul>
+          <a href="https://github.com/Djordannn">
+            <h2 className="bg-[#b1d690] p-3 rounded-lg text-black text-lg">
+              <FaGithub />
+            </h2>
+          </a>
         </div>
       </div>
       <div
@@ -165,31 +137,31 @@ export default function Home() {
         className="flex justify-center items-center h-[600px] bg-[#3c3c3c]"
       >
         <div className="text-center flex flex-col gap-2">
-          <h1 className="text-4xl tracking-wide">
-            Hello I&apos;am <span className="text-[#B1D690]">Djordan</span>
+          <h1 className="text-3xl md:text-4xl tracking-wide">
+            Hello I'am <span className="text-[#B1D690]">Djordan</span>
           </h1>
-          <h2 className="text-2xl tracking-wider">Web Developer</h2>
+          <h2 className="text-xl md:text-2xl tracking-wider">Web Developer</h2>
           <p className="w-[70%] mx-auto text-sm">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde
             accusamus commodi ipsa. Facilis, dicta mollitia!
           </p>
-          <Button className="block w-[15%] mx-auto mt-3 bg-[#B1D690] text-[#2d2d2d] hover:bg-[#b1d690]">
-            More
+          <Button className="block w-[20%] md:w-[15%] mx-auto mt-3 bg-[#B1D690] text-[#2d2d2d] hover:bg-[#b1d690]">
+            <a href="#myProject">More</a>
           </Button>
         </div>
       </div>
       <div
         id="about"
-        className="bg-[#B1D690] text-[#2d2d2d] mx-[12%] relative bottom-24 shadow-lg flex items-start gap-4 p-14"
+        className="bg-[#B1D690] text-[#2d2d2d] mx-[6%] md:mx-[12%] relative bottom-24 shadow-lg flex flex-col md:flex-row md:items-start gap-4 p-8 md:p-14"
       >
-        <div className="w-[30%] ">
-          <h1 className="text-5xl">
+        <div className="md:w-[30%] ">
+          <h1 className="text-center md:text-left text-4xl md:text-5xl">
             About <br />
             Me
           </h1>
         </div>
-        <div className="w-[70%]">
-          <p className="text-justify">
+        <div className="md:w-[70%]">
+          <p className="text-justify text-sm">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ex
             perspiciatis expedita modi suscipit maxime quod, est id, mollitia
             quos libero ducimus nesciunt nisi temporibus quasi ad alias commodi
@@ -197,28 +169,32 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div id="myProject" className="px-[9%] h-[400px] py-14">
-        <h1 className="text-2xl">My Project</h1>
-        <hr className="bg-[#B1D690] h-2 w-[10%] rounded-lg mt-3" />
-        <div>
-          <Carousel className="grid py-14 gap-6">
-            <CarouselContent>{newDataProject}</CarouselContent>
-            <CarouselPrevious className="text-black" />
-            <CarouselNext className="text-black" />
-          </Carousel>
+      <div id="myProject">
+        <div className="px-[9%] h-[400px] -mt-[7rem] md:-mt-[5rem] pt-[7rem] pb-14">
+          <h1 className="text-2xl">My Project</h1>
+          <hr className="bg-[#B1D690] h-2 w-[20%] md:w-[10%] rounded-lg mt-3" />
+          <div className="p-6 md:p-[2]">
+            <Carousel className="grid py-14 gap-6">
+              <CarouselContent>{newDataProject}</CarouselContent>
+              <CarouselPrevious className="text-black" />
+              <CarouselNext className="text-black" />
+            </Carousel>
+          </div>
         </div>
       </div>
-      <div id="tools" className="px-[9%] pt-[16rem]">
+      <div id="tools" className="px-[9%] mt-[18rem]">
         <h1 className="text-2xl">My Tools</h1>
-        <hr className="bg-[#B1D690] h-2 w-[10%] rounded-lg mt-3" />
-        <div className="py-14 grid grid-cols-4 gap-4">{newDataTool}</div>
+        <hr className="bg-[#B1D690] h-2 w-[20%] md:w-[10%] rounded-lg mt-3" />
+        <div className="py-14 grid grid-cols-2 md:grid-cols-4 gap-4">
+          {newDataTool}
+        </div>
       </div>
       <div id="footer" className="py-14 bg-[#3c3c3c]">
         <div className="flex flex-col gap-4 items-center">
           <h1 className="text-4xl">
             Get in <span className="text-[#b1d690]">Touch</span>
           </h1>
-          <p className="text-sm">
+          <p className="w-[90%] mx-auto text-center text-sm">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
             dolorum!
           </p>
